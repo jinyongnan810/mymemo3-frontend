6754ab1b32522bdc7738661a535ad393a82f185f
Kin Yuunan <yuunan.kin@gmail.com>
2021-12-26 12:39:07 +0900

    fix show error

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -10,90 +10,89 @@ const jsonConfig = {
   },
 };
 
-export const loadUser = (): ThunkAction<
-  void,
-  Object,
-  unknown,
-  AnyAction
-> => async (dispatch: Dispatch<Action>) => {
-  try {
-    const res = await axios.get("/api/users/currentuser");
-    if (res.data.currentUser) {
-      dispatch({
-        type: types.USER_LOADED,
-        payload: res.data.currentUser,
-      });
-    } else {
+export const loadUser =
+  (): ThunkAction<void, Object, unknown, AnyAction> =>
+  async (dispatch: Dispatch<Action>) => {
+    try {
+      const res = await axios.get("/api/users/currentuser");
+      if (res.data.currentUser) {
+        dispatch({
+          type: types.USER_LOADED,
+          payload: res.data.currentUser,
+        });
+      } else {
+        dispatch({
+          type: types.AUTH_EXPIRED,
+        });
+      }
+    } catch (error) {
       dispatch({
         type: types.AUTH_EXPIRED,
       });
     }
-  } catch (error) {
-    dispatch({
-      type: types.AUTH_EXPIRED,
-    });
-  }
-};
+  };
 
-export const login = (
-  email: string,
-  password: string
-): ThunkAction<void, Object, unknown, AnyAction> => async (dispatch) => {
-  try {
-    const res = await axios.post(
-      "/api/users/signin",
-      { email, password },
-      jsonConfig
-    );
-    dispatch({
-      type: types.LOGIN_SUCCESS,
-      payload: res.data,
-    });
-  } catch (error) {
-    console.error(error);
-    error.response.data.errors.map((e: string) => {
-      dispatch(showMessages(e, "danger"));
-    });
-  }
-};
+export const login =
+  (
+    email: string,
+    password: string
+  ): ThunkAction<void, Object, unknown, AnyAction> =>
+  async (dispatch) => {
+    try {
+      const res = await axios.post(
+        "/api/users/signin",
+        { email, password },
+        jsonConfig
+      );
+      dispatch({
+        type: types.LOGIN_SUCCESS,
+        payload: res.data,
+      });
+    } catch (error) {
+      console.error(JSON.stringify(error));
+      error.response.data.errors.map(
+        (e: { message: string; field: string }) => {
+          dispatch(showMessages(e.message, "danger"));
+        }
+      );
+    }
+  };
 
-export const signup = (
-  email: string,
-  password: string
-): ThunkAction<void, Object, unknown, AnyAction> => async (dispatch) => {
-  try {
-    const res = await axios.post(
-      "/api/users/signup",
-      { email, password },
-      jsonConfig
-    );
-    dispatch({
-      type: types.SIGNUP_SUCCESS,
-      payload: res.data,
-    });
-  } catch (error) {
-    console.error(error);
-    error.response.data.errors.map((e: string) => {
-      dispatch(showMessages(e, "danger"));
-    });
-  }
-};
+export const signup =
+  (
+    email: string,
+    password: string
+  ): ThunkAction<void, Object, unknown, AnyAction> =>
+  async (dispatch) => {
+    try {
+      const res = await axios.post(
+        "/api/users/signup",
+        { email, password },
+        jsonConfig
+      );
+      dispatch({
+        type: types.SIGNUP_SUCCESS,
+        payload: res.data,
+      });
+    } catch (error) {
+      console.error(error);
+      error.response.data.errors.map((e: string) => {
+        dispatch(showMessages(e, "danger"));
+      });
+    }
+  };
 
-export const logout = (): ThunkAction<
-  void,
-  Object,
-  unknown,
-  AnyAction
-> => async (dispatch) => {
-  try {
-    axios.post("/api/users/signout");
-    dispatch({
-      type: types.LOGOUT,
-    });
-  } catch (error) {
-    dispatch({
-      type: types.AUTH_EXPIRED,
-    });
-  }
-  // dispatch(clearMessages());
-};
+export const logout =
+  (): ThunkAction<void, Object, unknown, AnyAction> => async (dispatch) => {
+    try {
+      axios.post("/api/users/signout");
+      dispatch({
+        type: types.LOGOUT,
+      });
+    } catch (error) {
+      dispatch({
+        type: types.AUTH_EXPIRED,
+      });
+    }
+    // dispatch(clearMessages());
+  };
--- src/app/components/layout/Alert.tsx
+++ src/app/components/layout/Alert.tsx
@@ -5,14 +5,14 @@ import { useAppSelector } from "../../../app/hooks";
 const Alert = ({ alert }: { alert: any }) => {
   const message = useAppSelector((state) => state.messages);
   useEffect(() => {
-    if (!message.message.trim()) return;
+    if(message.message==="")return;
     if (message.level === "success") {
       alert.success(
         <div style={{ textTransform: "initial" }}>{message.message}</div>
       );
     } else {
-      alert.error(`${message.message}`);
-      console.error(`${message.message}`);
+      alert.error(message.message);
+      console.error(JSON.stringify(message));
     }
   }, [message]);
   return <div></div>;
